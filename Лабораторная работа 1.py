19c19f3af8a80d3961b98a825541cbd32d29767e
marymaksimova <115580839+marymaksimova@users.noreply.github.com>
2022-10-11 21:03:18 +0300

    Лабораторная работа 1

---
+++ Лабораторная работа 1.py
@@ -0,0 +1,4 @@
+Python 3.10.7 (v3.10.7:6cc6b13308, Sep  5 2022, 14:02:52) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("Hello World")
+Hello World
